874bea9b58eeef33c96ece589d169a67ac8ea1a5
seakee <seakee23@gmail.com>
2024-07-23 16:01:25 +0800

    Add copyright notice and package description

--- redis.go
+++ redis.go
@@ -1,3 +1,8 @@
+// Copyright 2024 Seakee.  All rights reserved.
+// Use of this source code is governed by a MIT style
+// license that can be found in the LICENSE file.
+
+// Package redis provides a Redis client implementation with connection pooling.
 package redis
 
 import (
